789eb6556422e9840ad968b02d371f04504be8d1
MiunoU145 <jeancamilocortina@gmail.com>
2026-03-23 15:26:21 -0500

    Enlaces conectados - Faltan los productos

--- js/productosadmin.js
+++ js/productosadmin.js
@@ -71,3 +71,28 @@ document.addEventListener("DOMContentLoaded", () => {
 function editarProducto(id){
     window.location.href = `editarproducto.html?id=${id}`;
 }
+
+// PARTE DEL FILTRO DE VALOR
+
+document.addEventListener("DOMContentLoaded", () => {
+    const range = document.getElementById("range4");
+    const output = document.getElementById("rangeValue");
+    const productos = document.querySelectorAll(".card");
+
+    //Mostrar el valor del rango
+    output.textContent = "$" + Number(range.value).toLocaleString("es-CO");
+
+    range.addEventListener("input", () => {
+        const valor = parseInt(range.value);
+        output.textContent = "$" + valor.toLocaleString("es-CO");
+
+        //Revisar cada producto
+        productos.forEach((producto) => {
+            const precio = parseInt(producto.dataset.precio);
+            const contenedor = producto.closest("article");
+
+            //Verificar si el precio es menor que el valor mostrado
+            contenedor.style.display = (precio <= valor) ? "" : "none";
+        });
+    });
+});
